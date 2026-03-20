bb4ff40a64a1ee03bba5b32d201df3d7f54c8c18
parijaiswal <parijaiswal346@gmail.com>
2026-03-20 16:28:57 +0530

    "Implement chatbot using python; add styling and HTML structure for chat interface."

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request, jsonify
+from groq import Groq
+
+app = Flask(__name__)
+GROQ_API_KEY = "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"
+client = Groq(api_key=GROQ_API_KEY)
+with open("knowledge.txt", "r", encoding="utf-8") as f:
+    knowledge = f.read()
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_input = request.json.get("message")
+    if user_input.lower() in ["hi", "hello", "hey", "hey there"]:
+        return jsonify({"reply": "Hello! How can I help you with Python and machine learning concepts"})
+    completion = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=[
+            {
+                "role":"user","content": f"Answer from this data: \n{knowledge}\n\nQuestion: {user_input}"
+            }
+        ]
+
+    )
+    reply = completion.choices[0].message.content
+    return jsonify({"reply": reply})
+if __name__ == "__main__":
+    app.run(debug=True)
